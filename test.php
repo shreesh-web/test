d0b6559b0328ec442b2ab993a39e7c8213482ce7
Shreesh Arora <shreesh.arora608@webkul.in>
2022-12-22 14:51:31 +0530

    crete function

--- test.php
+++ test.php
@@ -1,4 +1,12 @@
 <?php
 echo ('hello dev');
 echo ('new changes in dev server');
+
+function testdemo()
+{
+    echo('all ok');
+    echo('will move this line to another file');
+}
+
+testdemo();
 ?>
